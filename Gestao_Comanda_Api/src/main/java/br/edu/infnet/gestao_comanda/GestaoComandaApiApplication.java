6094d1d7a67975cbf144eb30896d0e98cb3df8f0
Jones <Jones@DESKTOP-N31076B>
2024-02-01 21:10:50 -0300

    Projeto Inicial

---
+++ Gestao_Comanda_Api/src/main/java/br/edu/infnet/gestao_comanda/GestaoComandaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gestao_comanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoComandaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoComandaApiApplication.class, args);
+	}
+
+}
---
+++ Gestao_Comanda_Api/src/test/java/br/edu/infnet/gestao_comanda/GestaoComandaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gestao_comanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoComandaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
